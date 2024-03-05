969009be6562d886ee167e590c92329d073ac362
Albionk1 <albionkrivenjeva1@gmail.com>
2024-03-05 14:52:53 +0100

    rregullimi i njoftimeve

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,4 +1,5 @@
 const Notification = require('../models/notificationModel')
+const User = require('../models/userModel')
 const { filterObj, validMongoId ,getUser} = require('../utils')
 const mongoose = require('mongoose')
 
@@ -25,11 +26,12 @@ const handleErrors = (err) => {
     const {by,to,message,from,status,url} = req.body
       const notification = await Notification.create({by,to,message,from,status,url})
       const user = await User.findById(by).select('full_name')
-      req.sendSocketMessage('notification', {user:{name:user.full_name,id:user._id},message,from,url})
+      req.sendSocketMessage('notification', {user:{name:user.full_name,id:user._id},message,from,url,date:notification.createdAt},to)
       res.send({status:'success',message:'Notification sent'})
       
   }
   catch(e){
+    console.log(e)
     res.send({status:'fail',message:'Something went wrong'})
 
   }
@@ -48,12 +50,30 @@ module.exports.deleteAllNotification = async(req,res)=>{
 
 module.exports.getNotifications=async(req,res)=>{
   try{
-     const {page}=req.params
-    const notifications = await Notification.find({to:req.user._id}).skip(parseInt(page)*5).limit(5)
-    console.log(notifications)
-    res.send(notifications)
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+    const skipIndex = (page - 1) * limit;
+     let userId =[]
+     let notificationIds = []
+    const notifications = await Notification.find({to:req.user._id,status:'delivered'}).skip(skipIndex).limit(5).lean().populate('by','full_name image')
+    for(let i =0;i<notifications.length;i++){
+      userId.push(notifications[i].by)
+      notificationIds.push(notifications[i]._id)
+    }
+    const hasMore = (skipIndex + limit) < await Notification.countDocuments({
+      to:req.user._id
+    });
+    await Notification.updateMany({_id:{$in:notificationIds}},{status:'seen'})
+    res.send({
+      status: 'success',
+      data: {
+        admin_notifications: notifications,
+        hasMore
+      },
+    })
   }
   catch(e){
+    console.log(e)
     res.send({status:'fail',message:'Something went wrong'})
   }
 }
