21f9635eb0d3a2af8eb1e8b2f4ce82917d8e7d61
unknown <albionkrivenjeva1@gmail.com>
2024-04-23 23:05:39 +0200

    rregullim i socketave

--- app.js
+++ app.js
@@ -133,7 +133,7 @@ app.use(socketMiddleware(socket))
         io.to(user.userId).emit('reciveMessageRoomMobile', { recivemessage: message,offert:false})
        return io.to(user.userId).emit('reciveMessageRoom', { recivemessage: message.message,offert:false})
       }
-      if (!user.room && user.userId) {
+      else{
         io.to(user.userId).emit('reciveMessageMobile', {myId:myId.toString(),recivemessage: message,offert:false })
         io.to(user.userId).emit('reciveMessage', {myId:myId.toString(), message:message.message,offert:false })
       }
