0e19158ba0fd0dad476a131bab562905ee8ba233
Albionk1 <albionkrivenjeva1@gmail.com>
2024-04-24 11:53:23 +0200

    rregullimi i heroku user notification

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -26,7 +26,7 @@ const handleErrors = (err) => {
     const {by,to,message,from,status,url} = req.body
       const notification = await Notification.create({by,to,message,from,status,url})
       const user = await User.findById(by).select('full_name')
-      req.sendSocketMessage('notification', {user:{name:user.full_name,id:user._id},message,from,url,date:notification.createdAt},to)
+      // req.sendSocketMessage('notification', {user:{name:user.full_name,id:user._id},message,from,url,date:notification.createdAt},to)
       res.send({status:'success',message:'Notification sent'})
       
   }
